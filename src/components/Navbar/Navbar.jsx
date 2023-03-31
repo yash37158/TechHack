998af709e76644999afcb975febcc095b0db5836
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2023-03-31 15:27:55 +0530

    added navbar comp

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css"
+
+export default function Navbar() {
+return (
+    <nav>
+        <div>
+            <li><a href=" ">Home</a></li>
+            <li><a href=" ">About</a></li>
+            <li><a href=" ">Dashboard</a></li>
+            <li><a href=" ">Contact Us</a></li>
+        </div>
+    </nav>
+);
+}
