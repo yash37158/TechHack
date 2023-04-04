25d81bfed028ba715e145a9ca3926c3ecc689c15
Deltacoderr <subodhinamdar99@gmail.com>
2023-04-04 15:55:15 +0530

    Added responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,34 @@
-import React from "react";
-import "./home.css"
+import { NavLink } from 'react-router-dom'
+import logo from '../../assets/logo.svg';
+import './navbar.css';
+import React from 'react';
 
-export default function Navbar() {
-return (
-    <nav>
-        <div>
-            <li><a href=" ">Home</a></li>
-            <li><a href=" ">About</a></li>
-            <li><a href=" ">Dashboard</a></li>
-            <li><a href=" ">Contact Us</a></li>
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+        <image src={logo} className='navbar-logo' />
         </div>
+        <div className="nav-elements">
+          <ul>
+            <li>
+              <NavLink to="/Hackathons">Hackathons</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Entertainment">Entertainment</NavLink>
+            </li>
+            <li>
+              <NavLink to="/About">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Rules">Rules & Navigations</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-);
+  )
 }
+
+export default Navbar
