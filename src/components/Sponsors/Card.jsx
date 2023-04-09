dcc6e7c226f4e2d16af7421fb388e96f70c91642
sachin3825 <sachinkumawat55@gmail.com>
2023-04-09 22:26:30 +0530

    card height

--- src/components/Sponsors/Card.jsx
+++ src/components/Sponsors/Card.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Card({ data }) {
   return (
-    <div className='bg-pink rounded-md w-[210px] h-[300px] flex flex-col gap-5 justify-center items-center sh'>
-      <div className='px-3'>
+    <div className='p-4 bg-pink rounded-md w-[12rem] h-[100px] flex flex-col gap-5 justify-center items-center sh'>
+      <div>
         <img src={data.logo} alt={data.title} className='object-fit' />
       </div>
 
