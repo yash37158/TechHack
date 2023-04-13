f0d2496d5b273fca10eece2558c5531c03d91305
AmanKeswani <keswaniaman137@gmail.com>
2023-04-13 12:28:45 +0530

    Made changes in the Connect section

--- src/components/Whatsapp/whatsapp.jsx
+++ src/components/Whatsapp/whatsapp.jsx
@@ -12,10 +12,10 @@ const Screen4 = () => {
                 <img className="object-cover object-center rounded" alt="hero" src={illustration4} />
               </div>
               <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                <h1 className="HeadingCo title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Connect with like-minded people and be part of a thriving community
+                <h1 className="HeadingCo title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Don't have a team yet?
                   <br className="hidden lg:inline-block" />
                 </h1>
-                <p className="ParaCo mb-8 leading-relaxed">Share your ideas, experiences, and insights with others who share your interests. Stay up-to date with group events and discussions, and make new connetions that will enrich your life.</p>
+                <p className="ParaCo mb-8 leading-relaxed">Share your ideas, experiences, insights with others who share your interests and find your team mates.</p>
                 <div className="flex justify-center">
                   <a href= "https://chat.whatsapp.com/ICPXbwfjK0T5Ka3GR6rwhk">
                     <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">Join our Whatsapp Group</button>
