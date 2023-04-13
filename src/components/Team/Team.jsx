682c8ef916f674c0ae8592646bf3a10dfe50b6a9
AmanKeswani <keswaniaman137@gmail.com>
2023-04-13 15:40:11 +0530

    Changed Team member's name

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -28,7 +28,7 @@ const Team = () => {
         <div class="h-full flex items-center border-indigo-950 border p-4 rounded-lg">
           <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={krithika}/>
           <div class="flex-grow">
-            <h2 class="name title-font font-medium">Krithika Manikanta</h2>
+            <h2 class="name title-font font-medium">Krithika Manikantan</h2>
             <p class="role"></p>
           </div>
         </div>
